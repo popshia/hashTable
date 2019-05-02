321dfd5d660d6179bcb21c3d709def91fea6985e
popshia <B123292179@gmail.com>
2019-05-02 16:05:11 +0800

    .

--- hashTable/main.cpp
+++ hashTable/main.cpp
@@ -1,4 +1,5 @@
-// 10627130 林冠良 & 10627131 李峻瑋 // CodeBlocks 17.12
+// 10627130 林冠良 & 10627131 李峻瑋 // Visual Studio Community 2019
+#define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -12,127 +13,173 @@
 #include <algorithm>
 #include <time.h>
 
-using namespace std ;
+using namespace std;
 
 typedef struct DataStruct {
-    unsigned char id ;
-    unsigned char name ;
-    unsigned char score ;
-    float average ;
-    
-} DataStruct ;
+    char id[10];
+    char name[10];
+    unsigned char score[6];
+    float average;
+    int hValue = 0;
+} DataStruct;
 
-static ifstream input ;
-static ofstream output ;
-static string FileNumber = "0" ;
-static int Count = 1 ;
-static int Command = 0 ;
-
-class function0 {
-    
-} ;
+static string FileNumber = "0";
+static int Count = 1;
+static int Command = 0;
+static fstream openTxt;
+static fstream openBin;
 
 class function1 {
+    DataStruct temp;
+public:
+    vector<DataStruct> dataBase;
     
-} ;
-
-class function2 {
-    
-} ;
-
-int main() {
-    bool continueOrNot = false ;
-    
-    do {
-        function0 Zero ;
-        function1 One ;
-        function2 Two ;
-        cout << "**********************************************" << endl ; // welcome message
-        cout << "*****               DS2ex03              *****" << endl ;
-        cout << "***** 0 : Quit                           *****" << endl ;
-        cout << "***** 1 : Read and analyze data          *****" << endl ;
-        cout << "***** 2 : Linear probing                 *****" << endl ;
-        cout << "***** 3 : Double hash                    *****" << endl ;
-        cout << "**********************************************" << endl ;
-        cout << endl << "Please enter your choice:" << endl ;
+    void ReadTxt() {
+        ;
+    }
+};
+                   
+class function2 : public function1 {
+public:
+    int FindPrimeNumber( int dataSize ) {
+        for ( bool findprime = false ; findprime != true ; dataSize++ ) {
+            for ( int i = 2 ; i <= dataSize ; i++ ) {
+                if ( dataSize % i == 0 ) {
+                    if ( dataSize == i ) findprime = true;
+                    else break;
+                }  // if
+            } // for
+        } // for
         
-        cin >> Command ; // read in user command
-        cout << endl ;
+        return dataSize;
+    } //FindPrimeNumber()
+    
+    int getHValue( char id[10] ) {
+        int hValue = 0 ;
+        for ( int i = 0 ; i < 7 ; i++ ) hValue = hValue * (int)id[i] ;
+        return hValue ;
+    } //getHvalue
+    
+    void BuildHashTableX() {
+        float size = dataBase.size() * 1.2;
+        DataStruct X[FindPrimeNumber((int)size)];
+        
+        for ( int i = 0 ; i < dataBase.size() ; i++ ) {
+            int tempHValue = getHValue( dataBase[i].id ) % (int)size ;
+            dataBase[i].hValue = tempHValue ;
+            X[tempHValue] = dataBase[i] ;
+        } // put into hashTable
+        
+    } // BuildHashTableX()
+    
+};
+                   
+class function3 : public function2 {
+                       
+};
+                   
+int main() {
+    bool continueOrNot = false;
+                       
+    do {
+        function1 One;
+        function2 Two;
+        function3 Three;
+        cout << "**********************************************" << endl; // welcome message
+        cout << "*****               DS2ex03              *****" << endl;
+        cout << "***** 0 : Quit                           *****" << endl;
+        cout << "***** 1 : Read and convert data          *****" << endl;
+        cout << "***** 2 : Linear probing                 *****" << endl;
+        cout << "***** 3 : Double hash                    *****" << endl;
+        cout << "**********************************************" << endl;
+        cout << endl << "Please enter your choice:" << endl;
+                           
+        cin >> Command; // read in user command
+        cout << endl;
         
         if ( Command == 0 ) { // bye :(((
-            cout << "Bye :(((" << endl ;
-            return 0 ;
+            cout << "Bye :(((" << endl;
+            return 0;
         } // quit
         
         else if ( Command > 3 || Command < 0 ) {
-            cout << "Error command! please enter an acceptable command:" << endl << endl ;
-            continueOrNot = true ;
+            cout << "Error command! please enter an acceptable command:" << endl << endl;
+            continueOrNot = true;
         } // wrong command
         
         else if ( Command == 1 ) { // read, count and build
-            bool function1Confirm = false ;
-            
+            bool function1Confirm = false;
+                               
             do {
-                cout << "Please enter the file you want to build a 2-3 Tree or [0] to quit:" << endl ;
-                cin >> FileNumber ;
-                
+                cout << "Please enter the file number you want to convert to binary file or [0] to quit:" << endl;
+                cin >> FileNumber;
+                                   
                 if ( FileNumber == "0" ) {
-                    function1Confirm = true ;
-                    continueOrNot = true ;
+                    function1Confirm = true;
+                    continueOrNot = true;
+                    cout << endl;
                 } // quit
                 
                 else {
-                    string txtFileName = "input" + FileNumber + ".txt" ;
-                    string binFileName = "input" + FileNumber + ".bin" ;
-                    input.open( txtFileName.c_str() ) ;
-                    if ( input.is_open() ) {
-                        // One.ReadNAnalyze() ;
-                        function1Confirm = true ;
-                        continueOrNot = true ;
+                    string txtFileName = "input" + FileNumber + ".txt";
+                    string binFileName = "input" + FileNumber + ".bin";
+                    //openBin = fopen( binFileName.c_str(), "rb" );
+                                       
+                    if ( openBin ) {
+                        //One.ReadBin();
+                        function1Confirm = true;
+                        continueOrNot = true;
+                    } // binary file open successfully
+                    
+                    else {
+                        //openTxt = fopen( txtFileName.c_str(), "r" );
+                        if ( openTxt ) {
+                            One.ReadTxt();
+                            function1Confirm = true;
+                            continueOrNot = true;
+                        } // txt file open sucessfully
+                        else {
+                            cout << "*****  file does not exist!  *****" << endl;
+                            function1Confirm = false;
+                        } // no such file
                     } // open successfully
-                    input.open( binFileName.c_str() ) ;
-                    if ( input.is_open() ) {
-                        // One.ReadNAnalyze() ;
-                        function1Confirm = true ;
-                        continueOrNot = true ;
-                    } // open successfully
-                    else cout << "*****  file does not exist!  *****" << endl ;
                 } // continue
-            } while( ! function1Confirm ) ;
-            
-            Count = 1 ;
-            FileNumber = "0" ;
-            input.close() ;
-            output.close() ;
-        } // 2-3 tree
-        
+            } while ( !function1Confirm );
+                               
+            Count = 1;
+            FileNumber = "0";
+            if ( openBin ) fclose( openBin );
+            if ( openTxt ) fclose( openTxt );
+        } // function 1
+                           
         else if ( Command == 2 ) {
-            bool function2Confirm = false ;
+            bool function2Confirm = false;
             
             do {
-                cout << "Please enter the file you want to build a AVL Tree or [0] to quit:" << endl ;
-                cin >> FileNumber ;
-                
+                cout << "Please enter the file you want to build a AVL Tree or [0] to quit:" << endl;
+                cin >> FileNumber;
+                                   
                 if ( FileNumber == "0" ) {
-                    function2Confirm = true ;
-                    continueOrNot = true ;
+                    function2Confirm = true;
+                    continueOrNot = true;
                 } // quit
-                
+                                   
                 else {
-                    string fileName = "input" + FileNumber + ".txt" ;
-                    input.open( fileName.c_str() ) ;
-                    if ( input.is_open() ) {
-                        function2Confirm = true ;
-                        continueOrNot = true ;
+                    string fileName = "input" + FileNumber + ".txt";
+                    if (1) {
+                        function2Confirm = true;
+                        continueOrNot = true;
                     } // open successfully
-                    else cout << "*****  " << fileName << " does not exist!  *****" << endl ;
+                    else cout << "*****  " << fileName << " does not exist!  *****" << endl;
                 } // open file and input data to BST
-            } while ( ! function2Confirm ) ;
-            
-            Count = 0 ;
-            FileNumber = "0" ;
-            input.close() ;
-            output.close() ;
+            } while ( !function2Confirm );
+                               
+            Count = 0;
+            FileNumber = "0";
         } // AVL Tree
-    } while( continueOrNot ) ;
-}
+                           
+        else if ( Command == 3 ) {
+            ;
+        }
+    } while ( continueOrNot );
+} // main()
